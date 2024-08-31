1925541e53e0a1bba127917e6162864894383ba5
Adhish-Krishna <adhishthesak@gmail.com>
2024-09-01 02:50:02 +0530

    Add uploading pdf and chatting with pdf support in the forntend

--- JavaScriptServer/index.js
+++ JavaScriptServer/index.js
@@ -2,7 +2,15 @@ import express from 'express'
 import pg from 'pg';
 import bodyParser from 'body-parser';
 import axios from 'axios';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import multer from 'multer';
+import path from 'path';
+import FormData from 'form-data';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT =3000;
 dotenv.config();
@@ -20,11 +28,21 @@ db.query("create table if not exists chat (id serial primary key, chatname text)
 db.query("create table if not exists chatmesages (id serial primary key, message text,response text,chatid integer references chat(id));");
 db.query("create table if not exists reviews (id serial primary key, name text, review text);");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
 let chat;
 let chatname;
 let chatnames;
 let curr_chatid;
-
 
 const app = express();
 app.use(express.static("public"));
@@ -66,6 +84,32 @@ app.get("/chats",async (req,res)=>{
   });
 })
 
+app.get("/pdfchats",async (req,res)=>{
+  const {chatid} = req.query;
+  curr_chatid = chatid;
+  chat = await db.query("select * from chatmesages where chatid=$1",[chatid]);
+  if(chat.rows.length==0){
+    chat = undefined;
+  }
+  res.render("chatwithpdf.ejs", {
+    chatnames:chatnames ? chatnames.rows : chatnames,
+    chatname: chatname ? chatname : undefined,
+    chat: chat ? chat.rows : undefined,
+  });
+})
+
+app.get("/chatwithpdf",async (req,res)=>{
+  chatnames = await db.query("select * from chat order by id desc");
+  if(chatnames.rows.length===0){
+    chatnames = undefined;
+  }
+  res.render("chatwithpdf.ejs", {
+    chatnames:chatnames ? chatnames.rows : chatnames,
+    chatname:undefined,
+    chat:undefined,
+  });
+})
+
 app.post("/newchat" , async (req,res)=>{
   const {chatname} = req.body;
   try {
@@ -78,12 +122,30 @@ app.post("/newchat" , async (req,res)=>{
 
 })
 
+app.post("/chatwithpdf" , async (req,res)=>{
+  const chatid = curr_chatid;
+  const message = req.body.message;
+  try{
+    const response = await axios.post("https://gbrh7rr7-5000.inc1.devtunnels.ms/upload/chat",{
+      question:message,
+      chat_id:curr_chatid
+    });
+    //must delete the pdf after the a response
+    await db.query("insert into chatmesages (message,response,chatid) values ($1,$2,$3)",[message,response.data.response,chatid]);
+    res.redirect(`/pdfchats?chatid=${chatid}`);
+  }catch(error){
+    console.log(error);
+    res.render("error404.ejs" , {error : "Bad Request"});
+
+  }
+})
+
 app.post("/chat" , async (req,res)=>{
   const chatid = curr_chatid;
   const message = req.body.message;
   try{
-    const response = await axios.post("http://localhost:5000/",{
-      question:message
+    const response = await axios.post("https://gbrh7rr7-5000.inc1.devtunnels.ms/",{
+      question:message,
     });
     await db.query("insert into chatmesages (message,response,chatid) values ($1,$2,$3)",[message,response.data.response,chatid]);
     res.redirect(`/chats?chatid=${chatid}`);
@@ -93,5 +155,29 @@ app.post("/chat" , async (req,res)=>{
 
   }
 })
+
+app.post('/uploadpdf', upload.single('pdfFile'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const filePath = path.join(__dirname, 'uploads', req.file.filename);
+  const chat_id = curr_chatid;
+  try {
+    const form = new FormData();
+    form.append('file', fs.createReadStream(filePath));
+    form.append('chat_id', chat_id);
+    const response = await axios.post('https://gbrh7rr7-5000.inc1.devtunnels.ms/upload', form, {
+      headers: {
+        ...form.getHeaders() // Include the form headers
+      },
+      timeout: 6000000
+    });
+    res.redirect(`/pdfchats?chatid=${chat_id}`);
+  } catch (error) {
+    console.error('Error sending file and chat_id to another API:', error);
+    res.status(500).send('Error sending file and chat_id to another API.');
+  }
+});
 
 app.listen(PORT,()=>console.log('Server is running'));
